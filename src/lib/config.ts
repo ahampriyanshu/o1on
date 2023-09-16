ad3cbbadc533f1167684619fa546753b884d6cc2
ahampriyanshu <ahampriyanshu@gmail.com>
2023-09-17 01:29:03 +0530

    docs: README.md

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment'
 
 export const title = 'O1ON'
-export const description = 'Personal Finance, simplified.'
+export const description = 'Personal finances, o(n)e click away'
 export const url = dev ? 'http://localhost:3000' : 'https://o1on.com'
